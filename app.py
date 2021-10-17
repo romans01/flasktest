0979f36f8c722aff5bb9f3944f60755923d5a34a
Roman Smirnov <smirnov.roman.a@gmail.com>
2021-10-17 18:07:22 +0300

    Adding docker

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p> Welcome to OS cluster!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=8001)
